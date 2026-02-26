ecb38d61465cf259949af7a11ec124c5d3a42d99
Alisher Tanirbergenov <alisher.tanirbergenov@stud.th-deg.de>
2026-02-26 13:11:18 +0100

    Adding C-1.26

---
+++ Python Primer/C/C-1.26.py
@@ -0,0 +1,9 @@
+
+a = int(input("Enter an integer: "))
+b = int(input("Enter an integer: "))
+c = int(input("Enter an integer: "))
+
+if a + b == c and a == b - c or a * b == c:
+    print("The numbers can be used")
+else:
+    print("Nope!")
